ca3618231aea6c3196b71349ff4afd32e10c884f
lucarl07 <junitinho2007@gmail.com>
2024-05-23 23:12:07 -0300

    Finish first functional version of the app

--- app/src/main/java/com/example/conversortemperatura/MainActivity.kt
+++ app/src/main/java/com/example/conversortemperatura/MainActivity.kt
@@ -1,20 +1,41 @@
 package com.example.conversortemperatura
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.conversortemperatura.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+       binding.btnConvert.setOnClickListener {
+           val celsius = binding.editCelsius.text.toString().toDoubleOrNull()
+           binding.returnCelsius.text = "$celsius ºC"
+
+           if (celsius != null) {
+               val fahrenheit = celsius * 1.8 + 32
+               binding.returnFahrenheit.text = "$fahrenheit ºF"
+           } else {
+               binding.returnCelsius.text = getString(R.string.nan_error)
+               binding.returnFahrenheit.text = getString(R.string.ellipsis)
+           }
+       }
     }
 }
